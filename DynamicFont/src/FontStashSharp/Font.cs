b1a313221b42255a7b330e4106678d1f42874053
Leandro Klyte <klyte45@hotmail.com>
2020-04-24 02:37:49 -0300

    Fixed uniform height text size calculation

--- DynamicFont/src/FontStashSharp/Font.cs
+++ DynamicFont/src/FontStashSharp/Font.cs
@@ -1,10 +1,11 @@
-﻿using StbTrueTypeSharp;
+﻿using Klyte.Commons.Utils;
+using StbTrueTypeSharp;
 
 namespace FontStashSharp
 {
     internal class Font
     {
-        private float AscentBase, DescentBase, LineHeightBase;
+        private float AscentBase, DescentBase, LineHeightBase, FontHeight;
 
         public float Ascent { get; private set; }
         public float Descent { get; private set; }
@@ -22,7 +23,17 @@ namespace FontStashSharp
         }
         public void RecalculateBasedOnHeight(float height)
         {
-            float size = height / (AscentBase - DescentBase);
+            float tempScale = _font.stbtt_ScaleForPixelHeight(100);
+            int aIdx = GetGlyphIndex(char.ConvertToUtf32("Á", 0));
+            int jIdx = GetGlyphIndex(char.ConvertToUtf32("j", 0));
+            int x0 = 0, y0 = 0, x1 = 0, y1 = 0;
+
+            _font.stbtt_GetGlyphBitmapBox(aIdx, tempScale, tempScale, ref x0, ref y0, ref x1, ref y1);
+            int maxY = y1;
+            _font.stbtt_GetGlyphBitmapBox(jIdx, tempScale, tempScale, ref x0, ref y0, ref x1, ref y1);
+            int minY = y0;
+
+            float size = height / (maxY - minY) * 45;
             Ascent = AscentBase * size;
             Descent = DescentBase * size;
             LineHeight = LineHeightBase * size;
@@ -50,10 +61,10 @@ namespace FontStashSharp
 
             font._font.stbtt_GetFontVMetrics(out int ascent, out int descent, out int lineGap);
 
-            int fh = ascent - descent;
-            font.AscentBase = ascent / (float)fh;
-            font.DescentBase = descent / (float)fh;
-            font.LineHeightBase = (fh + lineGap) / (float)fh;
+            font.FontHeight = ascent - descent;
+            font.AscentBase = ascent / font.FontHeight;
+            font.DescentBase = descent / font.FontHeight;
+            font.LineHeightBase = (font.FontHeight + lineGap) / font.FontHeight;
 
             return font;
         }
