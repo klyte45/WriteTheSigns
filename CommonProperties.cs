393e58d685da437a1fd349551e48ead05d31f887
Leandro Klyte <klyte45@hotmail.com>
2021-03-29 22:22:05 -0300

    Copying mod files to the staging folder Closes #5

--- CommonProperties.cs
+++ CommonProperties.cs
@@ -4,11 +4,18 @@ namespace Klyte.Commons
 {
     public static class CommonProperties
     {
+        internal static readonly string[] AssetExtraDirectoryNames = new string[0];
+        internal static readonly string[] AssetExtraFileNames = new string[] {
+            WTSController.m_defaultFileNameBuildingsXml+".xml",
+            WTSController.m_defaultFileNamePropsXml+".xml",
+            WTSController.m_defaultFileNameVehiclesXml+".xml"
+        };
+
         public static bool DebugMode => WriteTheSignsMod.DebugMode;
         public static string Version => WriteTheSignsMod.Version;
         public static string ModName => WriteTheSignsMod.Instance.SimpleName;
         public static string Acronym { get; } = "WTS";
-        public static string ModRootFolder     { get; } = WTSController.FOLDER_NAME;
+        public static string ModRootFolder { get; } = WTSController.FOLDER_NAME;
         public static string ModDllRootFolder { get; } = WriteTheSignsMod.RootFolder;
         public static string ModIcon => WriteTheSignsMod.Instance.IconName;
     }
